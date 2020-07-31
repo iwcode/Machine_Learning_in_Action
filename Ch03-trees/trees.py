8aaa7d4a6a1b11e46ea053af4810e807c2e2d7d1
iwcode <iwcode@wcc.fun>
2020-07-31 21:03:27 +0800

    update trees

--- Ch03-trees/trees.py
+++ Ch03-trees/trees.py
@@ -8,6 +8,7 @@
 
 
 from math import log
+import operator
 
 
 # 计算香农熵
@@ -30,3 +31,65 @@ def create_data_set():
     data_set = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
     return data_set, labels
+
+
+def split_data_set(data_set, axis, value):
+    ret_data_set = []
+    for feat_vec in data_set:
+        if feat_vec[axis] == value:
+            reduced_feat_vec = feat_vec[:axis]
+            reduced_feat_vec.extend(feat_vec[axis + 1:])
+            ret_data_set.append(reduced_feat_vec)
+    return ret_data_set
+
+
+def choose_best_feature_to_split(data_set):
+    num_features = len(data_set[0]) - 1
+    base_entropy = calc_shannon_ent(data_set)
+    best_info_gain = 0.0
+    best_feature = -1
+    for i in range(num_features):
+        feat_list = [example[i] for example in data_set]
+        unique_values = set(feat_list)
+        new_entropy = 0.0
+        for value in unique_values:
+            sub_data_set = split_data_set(data_set, i, value)
+            prob = len(sub_data_set) / float(len(data_set))
+            new_entropy += prob * calc_shannon_ent(sub_data_set)
+        info_gain = base_entropy - new_entropy
+        if info_gain > best_info_gain:
+            best_feature = i
+            best_info_gain = info_gain
+    return best_feature
+
+
+def majority_cnt(class_list):
+    class_count = {}
+    for vote in class_list:
+        if vote not in class_count.keys():
+            class_count[vote] = 0
+        class_count[vote] += 1
+    sorted_class_count = sorted(class_count.items(),
+                                key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
+def creat_tree(data_set, labels):
+    class_list = [example[-1] for example in data_set]
+    if class_list.count(class_list[0]) == len(class_list):
+        return class_list[0]
+    if len(data_set[0]) == 1:
+        return majority_cnt(class_list)
+    best_feat = choose_best_feature_to_split(data_set)
+    best_feat_label = labels[best_feat]
+    my_tree = {best_feat_label: {}}
+    del(labels[best_feat])
+    feat_values = [example[best_feat] for example in data_set]
+    unique_values = set(feat_values)
+    for value in unique_values:
+        sub_labels = labels[:]
+        my_tree[best_feat_label][value] = creat_tree(
+            split_data_set(data_set, best_feat, value), sub_labels)
+    return my_tree
+
+
